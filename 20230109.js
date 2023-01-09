e54411654bf9514059c5929e9a01b9a561c317ba
Pedro Marins <contato@pedromarins.com>
2023-01-09 18:28:01 -0300

    TIL: Javascript made me work to find the prime

---
+++ 20230109.js
@@ -0,0 +1,30 @@
+Lang: JavaScript
+Type: fastest
+
+Problem: 
+
+You must output the greatest prime number out of a list of N numbers.
+
+Input
+Line 1: An integer N for the amount of numbers to compare.
+Next N lines: An integer x
+
+Output
+Line 1 : The greatest prime number.
+
+Constraints
+2 ≤ N ≤ 100
+2 < x < 10000
+
+Example
+Input
+2
+7
+13
+
+Output
+13
+
+
+Solution: 
+
