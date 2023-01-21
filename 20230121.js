ec211ba6816e9553b8423e59a5469506e2bbff55
Pedro Marins <contato@pedromarins.com>
2023-01-21 17:28:16 -0300

    TIL: remembering the formula to calculate square meters

---
+++ 20230121.js
@@ -0,0 +1,38 @@
+Lang: JavaScript
+Type: fastest
+
+Problem: 
+
+You must print true (or false) if you have enough paint to cover the whole inner surface of the house.
+The paint stock is Q in liters.
+There are N rooms in the house.
+We'll' assume that 1L of paint will cover 5m².
+In each room, walls, floor and ceiling must be painted (with one layer).
+Don't mind about doors and windows, paint them all!
+
+Input
+Line1 : The paint stock in liters Q
+Line2 : The number of rooms to be painted N
+N next lines : X, Y, Z are the room dimensions in meters
+
+Output
+true if you have enough paint to cover the whole inner surface, false if not.
+
+Constraints
+N >= 0
+X, Y, Z <= 9
+Example
+
+Input
+10
+2
+3 4 3
+2 4 2
+
+Output
+false
+
+
+
+Solution: 
+
