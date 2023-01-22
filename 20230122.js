5aaa2aa1850d899cd57fa217209eb1977b837095
Pedro Marins <contato@pedromarins.com>
2023-01-22 19:23:18 -0300

    TIL: Better way to do a double loop

---
+++ 20230122.js
@@ -0,0 +1,67 @@
+Lang: JavaScript
+Type: reverse
+
+
+Problem: 
+
+The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
+
+01 Test 1
+Input
+1
+Expected output
+#
+
+02 Test 2
+Input
+3
+Expected output
+#
+##
+###
+ ##
+  #
+
+03 Test 3
+Input
+6
+Expected output
+#
+##
+###
+####
+#####
+######
+ #####
+  ####
+   ###
+    ##
+     #
+04 Test 4
+Input
+10
+Expected output
+#
+##
+###
+####
+#####
+######
+#######
+########
+#########
+##########
+ #########
+  ########
+   #######
+    ######
+     #####
+      ####
+       ###
+        ##
+         #
+
+
+
+Solution: 
+
