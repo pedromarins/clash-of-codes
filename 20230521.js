56677add57c8477839124683496fb38fc693058e
Pedro Marins <contato@pedromarins.com>
2023-05-22 05:05:25 -0300

    Create 20230521.js
    Clash of Code - Reverse Mode

---
+++ 20230521.js
@@ -0,0 +1,17 @@
+Lang: JavaScript
+Type: reverse
+
+Problem: 
+
+Input: 
+String
+
+Output:
+6 gnirtS
+
+
+Solution: 
+
+const input = readline().split('');
+
+console.log(input.length + " " + input.reverse().join(''));
